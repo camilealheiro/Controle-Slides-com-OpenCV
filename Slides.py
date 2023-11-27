ec50332576a09232e51a3d8f32db931ed09f82ce
Camile Alheiro <camilealheiro22@gmail.com>
2023-11-27 01:48:20 -0300

    Criação do indentificador de gestos que irão passar e voltar os slides

--- Slides.py
+++ Slides.py
@@ -1,48 +1,82 @@
 import cv2
 import mediapipe as mp
+import math
+
+mp_drawing = mp.solutions.drawing_utils
+drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+setaDir = cv2.imread("seta-direita.jpg")
+setaEsq = cv2.imread("seta-esquerda.jpg")
 
 video = cv2.VideoCapture(0)
 
-hand = mp.solutions.hands
-mao = hand.Hands(max_num_hands = 1)
+with mp_hands.Hands(
+    model_complexity = 0,
+    min_detection_confidence = 0.5,
+    min_tracking_confidence = 0.5) as hands:
 
-mpDraw = mp.solutions.drawing_utils
+    while video.isOpened():
+        ret, frame = video.read()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.flip(image, 1)
+
+        image.flags.writeable = False
+
+        results = hands.process(image)
+        image.flags.writeable = True
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        print(results)
+
+        lml = []
+        xl = []
+        yl = []
+        handLandmarks = []
+        if results.multi_hand_landmarks:
+            for hand_landmark in results.multi_hand_landmarks:
+
+                for landamrks in hand_landmark.landmark:
+                    handLandmarks.append([landamrks.x, landamrks.y])
 
 
-while True:
-    check, img = video.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = mao.process(imgRGB)
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
+                
+            for id, lm in enumerate(results.multi_hand_landmarks[0].landmark):
+                h, w,_ = image.shape
+                xc, yc = int(lm.x * w), int(lm.y * h)
+                lml.append([id, xc, yc])
+                xl.append(xc)
+                yl.append(yc)
 
-    handsPoints = results.multi_hand_landmarks
+            x1, y1 = lml[4][1], lml[4][2]
+            x2, y2 = lml[8][1], lml[8][2]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-    h, w, _ = img.shape
+            cv2.circle(image, (x1, y1), 10, (255, 0, 128), cv2.FILLED)
+            cv2.circle(image, (x2, y2), 10, (255, 0, 128), cv2.FILLED)
+            cv2.line(image, (x1, y1), (x2, y2), (255, 0, 128), 3)
 
-    pontos = []
+            distance = math.hypot(x2 - x1, y2 - y1)
+            cv2.putText(image, str(int(distance)), (cx + 30, cy), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 128), 3)
 
-    if handsPoints:
-        for points in handsPoints:
-            print(points)
-            mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
 
-            for id, cord in enumerate(points.landmark):
-                cx, cy = int(cord.x*w), int(cord.y*h)
-                cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                pontos.append((cx, cy))
+            if distance >= 200:
+                cv2.putText(image, "Next", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 3)
+            if handLandmarks[20][1] < handLandmarks[18][1]:
+                cv2.putText(image, "Back", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 3)
+
         
-        #Apenas mão direita
-        dedos = [8, 12, 16, 20]
-        contador = 0
-        if points:
-            if pontos[4][0] > pontos[2][0]:
-                contador += 1
+        cv2.imshow("video", image)
 
-            for x in dedos:
-                if pontos[x][1] < pontos[x-2][1]:
-                    contador += 1
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-        cv2.putText(img, str(contador), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 5)
 
-    cv2.imshow("Vídeo", img)
-    cv2.waitKey(1)
-
+video.release()
+cv2.destroyAllWindows()
